93793b50d3b977824a1abc1ba3c221747054c624
Victor <vavillar@espol.edu.ec>
2025-07-30 15:28:08 -0500

    Proyecto subido

---
+++ CalculatorSMP/src/main/java/espol/calculatorsmp/CalculatorSMP.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculatorsmp;
+
+/**
+ *
+ * @author Djurado
+ */
+public class CalculatorSMP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        String question = Operations.MakeFormula();
+        System.out.println(question);
+        String ret = Operations.Solve(question);
+        System.out.println(ret);
+    }
+    
+}
